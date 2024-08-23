4a23c23bd82d1936c45de2f04f41e48d8ae8077d
mechanikate <cjmatthy200@gmail.com>
2024-08-23 00:17:07 -0400

    im gonna lose it

--- update.sh
+++ update.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 rm -r paperclips
-mkdir paperclips
-cd paperclips
 
 wget -m http://www.decisionproblem.com/paperclips/
 
-mkdir -p docs
-mv www.decisionproblem.com/paperclips/* docs
+mkdir -p original 
+mv www.decisionproblem.com/paperclips/* original
 rm -rf www.decisionproblem.com
 find . -name '*\?*' | while read -r path ; do
     mv "$path" "${path%\?*}"
 done
 
-cd ..
-
-diff -crB paperclips/docs/ ./ > dfile.patch
+diff -crB original/ ./ > dfile.patch
